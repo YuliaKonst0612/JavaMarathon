20fc97abc18c404d9141790856b3a47436efa577
Your Name <yulia1988const@gmail.com>
2023-03-16 16:19:00 +0300

    completed tasks day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+Car car = new Car();
+car.setName("BMW");
+car.setColor("black");
+car.setYear(1985);
+        System.out.println(car.getName());
+        System.out.println(car.getColor());
+        System.out.println(car.getYear());
 
+    }
+}
+
+class Car {
+    private String name;
+    private String color;
+    private int year;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+       this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+       this.year = year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,48 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike  motorbike = new  Motorbike("Harley", "red", 1968);
+        System.out.println(motorbike.getName());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getYear());
 
     }
 }
+
+class Motorbike {
+    private String name;
+    private String color;
+    private int year;
+
+    public Motorbike(String name, String color, int year) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+
