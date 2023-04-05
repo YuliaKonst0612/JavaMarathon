d7a002d1f35b1b6859141c2fe2f4b40a33c76053
Your Name <yulia1988const@gmail.com>
2023-04-05 15:45:42 +0300

    completed tasks day_16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        printResult();
+    }
 
+    public static void printResult() {
+        File file = new File("test1");
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+
+            int num = 0;
+            for (String s : numbers)
+                num += Integer.parseInt(s);
+
+            double result = num / (double) numbers.length;
+            System.out.printf(result + "-->%.3f ", result);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,40 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file1 = new File("src/main/resources/file1.txt");
+        PrintWriter pw = new PrintWriter(file1);
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++)
+            pw.println(random.nextInt(100));
+        pw.close();
+
+        Scanner sc = new Scanner(file1);
+        File file2 = new File("src/main/resources/file2.txt");
+        PrintWriter pw1 = new PrintWriter(file2);
+        int counter = 0;
+        int sum = 0;
+        while (sc.hasNextLine()) {
+            sum += Integer.parseInt(sc.nextLine());
+            counter++;
+            if (counter == 20) {
+                pw1.println(sum / 20.0);
+                counter = 0;
+                sum = 0;
+            }
+        }
+        pw1.close();
+
+        Scanner sc2 = new Scanner(file2);
+        double result = 0;
+        while (sc2.hasNextLine())
+            result += Double.parseDouble(sc2.nextLine());
+        System.out.println((int) result);
     }
 }
