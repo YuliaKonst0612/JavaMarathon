bb41c78410832beeded29c8d6439eb7a522b8882
Your Name <yulia1988const@gmail.com>
2023-04-05 12:17:24 +0300

    completed tasks day_15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,31 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        try {
+            File file = new File("src/main/resources/shoes.csv");
+            File file1 = new File("src/main/resources/missing shoes.txt");
+            // file.getParentFile().mkdirs();
+            PrintWriter printWriter = new PrintWriter(file1);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String[] shoes = scanner.nextLine().split(";");
+                if (Integer.parseInt(shoes[2]) == 0) {
+                    printWriter.println(shoes[0] + " " + shoes[1] + " " + shoes[2]);
+                }
+            }
+            printWriter.close();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("incorrect data input in the file");
+        }
     }
 }
