a7bbb88736bf271e92f32186d29eb9dc44677416
Your Name <yulia1988const@gmail.com>
2023-04-11 12:22:22 +0300

    completed tasks day_17_another solving

--- src/main/java/day17/ChessBoard.java
+++ src/main/java/day17/ChessBoard.java
@@ -1,19 +1,43 @@
 package day17;
 
+
+import java.awt.*;
+import java.util.Map;
+
 public class ChessBoard {
-    private ChessPiece[][] board;
 
-    public ChessBoard(ChessPiece[][] board) {
+    private Map<Point, ChessPiece> board;
+
+    public ChessBoard(Map<Point, ChessPiece> board) {
         this.board = board;
-
     }
 
     public void printBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j].getPicture());
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Point point = new Point(i, j);
+                ChessPiece piece = board.getOrDefault(point, ChessPiece.EMPTY);
+                System.out.print(piece.getPicture());
             }
             System.out.println();
         }
     }
 }
+
+//    private ChessPiece[][] board;
+//
+//    public ChessBoard(ChessPiece[][] board) {
+//        this.board = board;
+//
+//    }
+//
+//    public void printBoard() {
+//        for (int i = 0; i < board.length; i++) {
+//            for (int j = 0; j < board.length; j++) {
+
+//                System.out.print(board[i][j].getPicture());
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -1,17 +1,48 @@
 package day17;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task2 {
     public static void main(String[] args) {
-        ChessBoard chessBoard = new ChessBoard(new ChessPiece[][]{
-                {ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
-                {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KNIGHT_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
-                {ChessPiece.QUEEN_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}
-        });
-chessBoard.printBoard();
+        Map<Point, ChessPiece> initialSetup = new HashMap<>();
+        initialSetup.put(new Point(0, 0), ChessPiece.ROOK_BLACK);
+        initialSetup.put(new Point(0, 5), ChessPiece.ROOK_BLACK);
+        initialSetup.put(new Point(0, 6), ChessPiece.KING_BLACK);
+        initialSetup.put(new Point(1, 1), ChessPiece.ROOK_WHITE);
+        initialSetup.put(new Point(1, 4), ChessPiece.PAWN_BLACK);
+        initialSetup.put(new Point(1, 5), ChessPiece.PAWN_BLACK);
+        initialSetup.put(new Point(1, 7), ChessPiece.PAWN_BLACK);
+        initialSetup.put(new Point(2, 0), ChessPiece.PAWN_BLACK);
+        initialSetup.put(new Point(2, 2), ChessPiece.KNIGHT_BLACK);
+        initialSetup.put(new Point(2, 6), ChessPiece.PAWN_BLACK);
+        initialSetup.put(new Point(3, 0), ChessPiece.QUEEN_BLACK);
+        initialSetup.put(new Point(3, 3), ChessPiece.BISHOP_WHITE);
+        initialSetup.put(new Point(4, 3), ChessPiece.BISHOP_BLACK);
+        initialSetup.put(new Point(4, 4), ChessPiece.PAWN_WHITE);
+        initialSetup.put(new Point(5, 4), ChessPiece.BISHOP_WHITE);
+        initialSetup.put(new Point(5, 5), ChessPiece.PAWN_WHITE);
+        initialSetup.put(new Point(6, 0), ChessPiece.PAWN_WHITE);
+        initialSetup.put(new Point(6, 3), ChessPiece.QUEEN_WHITE);
+        initialSetup.put(new Point(6, 5), ChessPiece.PAWN_WHITE);
+        initialSetup.put(new Point(6, 7), ChessPiece.PAWN_WHITE);
+        initialSetup.put(new Point(7, 5), ChessPiece.ROOK_WHITE);
+        initialSetup.put(new Point(7, 6), ChessPiece.KING_WHITE);
+
+        ChessBoard chessBoard = new ChessBoard(initialSetup);
+        chessBoard.printBoard();
     }
 }
+
+//         ChessBoard chessBoard = new Map<Point, ChessBoard>(new ChessPiece[][]{
+//
+//                {ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
+//                {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KNIGHT_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
+//                {ChessPiece.QUEEN_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}
+//        });
